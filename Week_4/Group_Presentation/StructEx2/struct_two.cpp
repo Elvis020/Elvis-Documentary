83e456e70207d7c15d19db69799dcb27e2827113
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2021-02-10 07:03:15 +0000

    Added some struct examples

--- Week_4/Group_Presentation/StructEx2/struct_two.cpp
+++ Week_4/Group_Presentation/StructEx2/struct_two.cpp
@@ -9,25 +9,25 @@ struct A{
     double j;
 };
 
+
+
 // Without intializers in Struct b
+
 struct B{
-    int i = 0;
-    double j = 0.0;
-    string name;
+    int i;
+    double j;
 };
 
-// Without intializers in Struct b
-
-
-struct C{
-    double price;
-    string name;
-}c={12.1,"Elvis"};
 
 // struct C{
 //     double price;
 //     string name;
-// }c,v;
+// }c={12.1,"Elvis"};
+
+struct C{
+    double price;
+    string name;
+}c,v;
 
 
 int main(){
@@ -37,24 +37,22 @@ int main(){
     // C c = {};
     // C c;
 
-   B b2,b3 = {};
-   B b1 = {1,9.8,"Elvis"};
+//    B b1 = {1,9.8,"Elvis"};
 //    price = {21.1};
 //    name = {"Elvis"};
 
 
 //    B b2,b3 = {};
-//    B b1 = {1,9.8,"Elvis"};
+   B b1 = {1,9.8};
     // cout << "Without  initialization" << endl;
     // cout << "i: " << a.i << " j: " << a.j << endl;
     // cout << "i1: " << a1.i << " j1: " << a1.j << endl;
 
     cout << "With initialization" << endl;
-    cout << "i: " << b1.i << " j: " << b1.j << b1.name << endl;
-    cout << "i1: " << b2.i << " j1: " << b3.j << endl;
+    cout << "i1: " << b.i << " j1: " << b.j << endl;
     // cout << "price: " << c.price << " name: " << c.name << endl;
     // cout << "price: " << c.price << " name: " << v.name << endl;
-    cout << "price: " << c.price << " name: " << c.name << endl;
+    // cout << "price: " << c.price << " name: " << c.name << endl;
 
 
     // To switch between g++ compilers:
