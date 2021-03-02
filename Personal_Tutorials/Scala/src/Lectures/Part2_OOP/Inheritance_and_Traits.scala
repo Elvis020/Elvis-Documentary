b043988cf2e96a885d2739523a8b2b3a0a03456d
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2021-03-02 20:22:00 +0000

    Adding some personal stuff

--- Personal_Tutorials/Scala/src/Lectures/Part2_OOP/Inheritance_and_Traits.scala
+++ Personal_Tutorials/Scala/src/Lectures/Part2_OOP/Inheritance_and_Traits.scala
@@ -1,8 +1,9 @@
 package Lectures.Part2_OOP
 
 object Inheritance_and_Traits extends App {
-  class Animal{
-    protected def eat():Unit  = println("nom - nom")
+  sealed class Animal {
+    val creatureType:String = "Wild";
+    def eat():Unit  = println("nom - nom")
 //    Protected means only use-able within class and subclasses
   }
 //  Single class inheritance
@@ -14,4 +15,36 @@ object Inheritance_and_Traits extends App {
 }
   val cat = new Cat
   cat.crunch
+
+
+
+//  Constructors
+  class Person(name:String, age:Int) {
+    def this(name:String) = this(name,0)
+}
+  class Adult(name:String, age:Int, idCard:String) extends Person(name)
+
+
+  class Dog(override val creatureType: String)  extends Animal {
+//    override val creatureType: String = "domestic"
+    override def eat ={
+      super.eat()
+      println("Doggy: crunch - crunch")
+    }
+  }
+
+  val d = new Dog("K6")
+  d.eat
+  println(d.creatureType)
+
+
+//  Type substitution
+  val unknownAnimal: Animal = new Dog("K9"); // Polymorphism
+  unknownAnimal.eat()
+
+
+//  To prevent overrides
+//  1 - use final on member
+//  2 - use final on the entire class
+//  3 - seal the class = extend classes in THIS FILE, prevent extension from another file
 }
