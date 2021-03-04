5d1fa568e2008ea8074070ab1eb8ef5a00b2264c
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2021-03-04 16:58:47 +0000

    Added q4

--- Labs/Week_7/Day_4/Functional_Scala_Labs/src/Collections_Labs.scala
+++ Labs/Week_7/Day_4/Functional_Scala_Labs/src/Collections_Labs.scala
@@ -2,8 +2,18 @@
 
 object Collections_Labs extends App {
   val file_structure = (new java.io.File(".")).listFiles.toList
-  val not_hidden_file_names: Unit = file_structure.filter(x => !x.toString.startsWith(raw""".\.""")).foreach(x => println(x))
-  val is_File = file_structure.partition(x => x.isFile); println(is_File)
-  val paired: Unit = file_structure filter(_.isFile) map(x => (x.getName,x.length)) foreach(x =>println(x))
+//  1. val not_hidden_file_names: Unit = file_structure.filter(x => !x.toString.startsWith(raw""".\.""")).foreach(x => println(x))
+//  2. val is_File = file_structure.partition(x => x.isFile); println(is_File)
+//  is_File.sortBy(x => x.length)
+//  3. val paired: Unit = file_structure filter(_.isFile) map(x => (x.getName,x.length)) foreach(x =>println(x))
+  println()
+  println("** All elements **")
+  val paired1: Unit = file_structure.filter(_.isFile).sortBy(x => x.length).map(x => (x.getName,x.length))foreach(x =>println(x))
+  println()
+  println("** First 10 elements **")
+  val paired2: Unit = file_structure.filter(_.isFile).sortBy(x => x.length).take(10).map(x => (x.getName,x.length))foreach(x =>println(x))
+  println()
+  println("** Last 10 elements **")
+  val paired3: Unit = file_structure.filter(_.isFile).sortBy(x => -x.length).take(10).map(x => (x.getName,x.length))foreach(x =>println(x))
 
 }
